19618e8bb51a231e47f93669e6f098e039cfce97
gustavohenriquerssilva <49284363+gustavohenriquerssilva@users.noreply.github.com>
2019-04-11 21:27:22 -0300

    Implementa exercício 5

--- aula-01/java/src/ex5.java
+++ aula-01/java/src/ex5.java
@@ -1,4 +1,18 @@
 
 public class ex5 {
-
+  public static void main(String[] args){
+		int x = 3;
+		int resultado = ObtenhaSomaPrimeirosNaturais(x);
+		System.out.println(resultado);
+	}
+	
+	private static int ObtenhaSomaPrimeirosNaturais(int x){
+		int i = 2;
+		int s = 1;
+		while(i <= x ){
+			s = s +  i;
+			i++;
+		}
+		return s;
+	}
 }
