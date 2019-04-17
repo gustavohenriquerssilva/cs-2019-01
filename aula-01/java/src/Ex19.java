a4331293a9d2a15996efecb47d99302eab6ea88e
gustavohenriquerssilva <49284363+gustavohenriquerssilva@users.noreply.github.com>
2019-04-16 21:39:16 -0300

    Adiciona IllegalArgumentException

--- aula-01/java/src/Ex19.java
+++ aula-01/java/src/Ex19.java
@@ -1,13 +1,16 @@
 
-public class ex19 {
+public class Ex19 {
 	public static void main(String[] args){
 		int n = 81;
-		int resultado = ObtenhaFibonacci(n);
+		int resultado = obtenhaFibonacci(n);
 		System.out.println(resultado);
 	}
 	
 	
-	private static int ObtenhaFibonacci(int n){
+	private static int obtenhaFibonacci(int n){
+		if(n < 0){
+			throw new IllegalArgumentException("É permitido somente valor inteiro");
+		}
 		int a = 0;
 		int c = 1;
 		if(n == 0 || n == 1){
