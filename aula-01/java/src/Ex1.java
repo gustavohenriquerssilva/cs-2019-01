571df7824ad3f3a1936ec5d1db6cf1dba9a0d819
gustavohenriquerssilva <49284363+gustavohenriquerssilva@users.noreply.github.com>
2019-04-16 21:05:32 -0300

    Altera nome do método para aderir ao padrão java

--- aula-01/java/src/Ex1.java
+++ aula-01/java/src/Ex1.java
@@ -8,7 +8,7 @@ public class Ex1 {
 		System.out.println(resultado);
 	}
 	
-	public static boolean EhPropriedade3025(int valorTestado){
+	public static boolean ehPropriedade3025(int valorTestado){
 		if(valorTestado > 9999 || valorTestado < 0){
 			throw new IllegalArgumentException("Valor inserido não pode ser menor que zero ou maior que 9999");
 		}
