0fb8ced6efe6a117fae4d602534fb02bcffa6236
Rebecca Moraes <rebecca@ufg.br>
2019-06-18 21:39:03 -0300

    Adiciona correções

--- aula-01/java/src/main/java/com/github/gustavohenriquerssilva/cs/aula1/application/console/ProgramaExercicio.java
+++ aula-01/java/src/main/java/com/github/gustavohenriquerssilva/cs/aula1/application/console/ProgramaExercicio.java
@@ -2,18 +2,20 @@ package main.java.com.github.gustavohenriquerssilva.cs.aula1.application.console
 
 import main.java.com.github.gustavohenriquerssilva.cs.aula1.domain.Exercicios;
 
-
+/**
+ * Ponto de entrada da aplicação. Apenas para ilustra chamada de método.
+ *
+ * @param args Ignorados.
+ */
 public final class ProgramaExercicio {
-	/**
-	 * Ponto de entrada da aplicação. Apenas para ilustra chamada de método.
-	 *
-	 * @param args Ignorados.
-	 */
-	public static void main(String[] args) {
-
-		System.out.println(Exercicios.ehNumeroPrimo(99));
-	}
-	
-	
+    /**
+     * Ponto de entrada da aplicação. Apenas para ilustra chamada de método.
+     *
+     * @param args Ignorados.
+     */
+    private static void main(final String[] args) {
+        final int valorEntrada = 99;
+        Exercicios.ehNumeroPrimo(valorEntrada);
+    }
 }
 
---
+++ aula-01/java/src/main/java/com/github/gustavohenriquerssilva/cs/aula1/application/console/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Pacote com a implementação das classes.
+ * {@link com.github.gustavohenriquerssilva.cs.aula01.Exercicios}
+ */
+package com.github.gustavohenriquerssilva.cs.aula01;
