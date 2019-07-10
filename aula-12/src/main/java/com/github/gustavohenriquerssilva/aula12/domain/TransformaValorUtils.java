b2540f8dd572cc0e3d8c72bc6d8da942eb38cdf0
gustavohenriquerssilva <gustavohenriquerssilva@gmail.com>
2019-07-10 03:43:33 -0300

    Adiciona classe que implementa método para transformar valor

---
+++ aula-12/src/main/java/com/github/gustavohenriquerssilva/aula12/domain/TransformaValorUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018.
+ * Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package com.github.gustavohenriquerssilva.aula12.domain;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Reúne a diferença entre datas
+ */
+public class TransformaValorUtils {
+
+    /**
+     * Construtor privado da classe.
+     */
+    private TransformaValorUtils() {
+
+    }
+
+    /**
+     * Obtém a diferença entre datas
+     *
+     * @param dataInicial Ignorados.
+     * @param dataFinal   Ignorados.
+     * @return Diferença de dias entre a data inicial e final
+     */
+    public static String getValorPorExtenso(String numero) {
+
+        return "";
+    }
+}
