62a9ededde54dfbc71112ce8172911be433ecffe
gustavohenriquerssilva <gustavohenriquerssilva@gmail.com>
2019-07-10 02:24:59 -0300

    Adiciona formatação

--- aula-11/gui/js/DiferencaDatas.js
+++ aula-11/gui/js/DiferencaDatas.js
@@ -1,56 +1,54 @@
-<script>
-        // Path para a requisição (URL)
-        const PATH = "http://localhost:8080/ds?dataFinal=";
+// Path para a requisição (URL)
+const PATH = "http://localhost:8080/ds?dataFinal=";
 
-        function atualizaDiaDaSemana() {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    let dds = extraiDiaDaSemanaDaResposta(xhttp.responseText);
-                    document.getElementById("resultado").innerHTML = dds;
-                }
-            };
-
-            let di = document.getElementById("dataFinalInicial").value;
-            let dataFinal = formataDataFinal(di);
-            let df = document.getElementById("dataFinalFinal").value;
-            let dataFinal = formataDataFinal(df);
-            xhttp.open("GET", monteURL(dataInicial, dataFinal), true);
-            xhttp.send();
+function atualizaDiaDaSemana() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let dds = extraiDiaDaSemanaDaResposta(xhttp.responseText);
+            document.getElementById("resultado").innerHTML = dds;
         }
+    };
 
-        function monteURL(dataInicial, dataFinal) {
-            return PATH + dataInicial + "&dataFinal=" + dataFinal;
-        }
+    let di = document.getElementById("dataFinalInicial").value;
+    let dataFinal = formataDataFinal(di);
+    let df = document.getElementById("dataFinalFinal").value;
+    let dataFinal = formataDataFinal(df);
+    xhttp.open("GET", monteURL(dataInicial, dataFinal), true);
+    xhttp.send();
+}
 
-        function dataFinalCorrente() {
-            document.getElementById("dataFinal").valueAsDate = new Date();
-        }
+function monteURL(dataInicial, dataFinal) {
+    return PATH + dataInicial + "&dataFinal=" + dataFinal;
+}
 
-        // Funções para integração (satisfazer contrato do servidor)
+function dataFinalCorrente() {
+    document.getElementById("dataFinal").valueAsDate = new Date();
+}
 
-        function extraiDiaDaSemanaDaResposta(resposta) {
-            return JSON.parse(resposta).diaDaSemana;
-        }
+// Funções para integração (satisfazer contrato do servidor)
 
-        // Dia ou mês deve possuir dois dígitos
-        function formataDiaOuMes(n) {
-            return ("00" + n).substr(-2, 2);
-        }
+function extraiDiaDaSemanaDaResposta(resposta) {
+    return JSON.parse(resposta).diaDaSemana;
+}
 
-        // Ano deve possuir quatro dígitos
-        function formataAno(n) {
-            return ("0000" + n).substr(-4,4);
-        }
+// Dia ou mês deve possuir dois dígitos
+function formataDiaOuMes(n) {
+    return ("00" + n).substr(-2, 2);
+}
 
-        // ENTRADA: ano-mes-dia SAIDA: dd-mm-yyyy
-        function formataDataFinal(dataFinal) {
-            let [a, m, d] = dataFinal.split("-");
+// Ano deve possuir quatro dígitos
+function formataAno(n) {
+    return ("0000" + n).substr(-4, 4);
+}
 
-            let dia = formataDiaOuMes(d);
-            let mes = formataDiaOuMes(m);
-            let ano = formataAno(a);
+// ENTRADA: ano-mes-dia SAIDA: dd-mm-yyyy
+function formataDataFinal(dataFinal) {
+    let [a, m, d] = dataFinal.split("-");
 
-            return `${d}-${m}-${a}`;
-        }
-    </script>
+    let dia = formataDiaOuMes(d);
+    let mes = formataDiaOuMes(m);
+    let ano = formataAno(a);
+
+    return `${d}-${m}-${a}`;
+}
